37dac2b0196132bfcb9c41c7ccf2e42542bde91a
Yudhishthir <yudhishthir.ongraph@gmail.com>
2021-04-23 13:12:52 +0530

    [Setup Devise]:  -Create views using devise  -Create model using devise  -Add links in layouts and static pages  -Configure development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Define host for mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Raise errors if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
