01d851a727014d3a3237638d3f226723cc8ea4be
Yudhishthir <yudhishthir.it21@gmail.com>
2021-05-06 20:16:32 +0530

    [Minor fix]:  -Minor fix in comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :micropost
   belongs_to :user
 
-  validates [:user_id, :micropost_id], presence: true
+  validates :user_id, presence: true
+  validates :micropost_id, presence: true
   validates :content, presence: true 
 end
